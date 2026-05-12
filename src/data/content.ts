076dd3605ad02851dcffd30f4d89ed0ce43a79f7
JasonLai150 <laijason150@gmail.com>
2026-05-12 15:55:28 -0400

    change link

--- src/data/content.ts
+++ src/data/content.ts
@@ -64,7 +64,7 @@ export const projectsData = [
   {
     title: "echoAI",
     description: `🏆 Podium for Google x Qualcomm Edge AI Hackathon. ${NEWLINE}Mobile app to help deaf and hard-of-hearing users detect, locate, and prioritize important sounds around them in real time, fully on-device. `,
-    technologies: ["liteRT", "tflite", "Android Studio", "Kotlin"],
+    technologies: ["liteRT", "Android Studio", "Kotlin", "Gradle"],
     link: "https://devpost.com/software/echoai-t7ol0v?ref_content=my-projects-tab&ref_feature=my_projects"
   },
   {
